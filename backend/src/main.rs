aade0f4aec23e89871e6fe581c21e76cfb897641
Xavier Loera Flores <x.loera.tech@gmail.com>
2025-04-25 00:24:07 -0700

    Implement basic Axum server with a root route and dynamic port configuration in main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(root));
+
+    // Get the port number from the environment, default to 3000
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string()) // Get the port as a string or default to "3000"
+        .parse() // Parse the port string into a u16
+        .expect("Failed to parse PORT");
+
+    // Create a socket address (IPv6 binding)
+    let address = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+
+    println!("Listening on PORT: {}", address.port());
+
+    // Run the app with hyper, listening on the specified address
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello World!"
 }
